84c5b72ee1fe5388d31f6dd88b7b972e7aac5646
Mazen Hassani <83642540+mazen-hassani@users.noreply.github.com>
2021-12-09 19:41:48 +0100

    Add files via upload

---
+++ knn.py
@@ -0,0 +1,231 @@
+import numpy as np
+"""
+Credits: the original code belongs to Stanford CS231n course assignment1. Source link: http://cs231n.github.io/assignments2019/assignment1/
+"""
+
+class KNN:
+    """ a kNN classifier with L2 distance """
+
+    def __init__(self, k=1, metric=None):
+        self.k = k
+        self.metric = metric
+
+    def fit(self, X, y):
+        """
+        Train the classifier. For k-nearest neighbors this is just
+        memorizing the training data.
+
+        Inputs:
+        - X: A numpy array of shape (num_train, D) containing the training data
+          consisting of num_train samples each of dimension D.
+        - y: A numpy array of shape (N,) containing the training labels, where
+             y[i] is the label for X[i].
+        """
+        self.train_X = X
+        self.train_y = y
+
+    def predict(self, X, num_loops=0):
+        """
+        Predict labels for test data using this classifier.
+
+        Inputs:
+        - X: A numpy array of shape (num_test, D) containing test data consisting
+             of num_test samples each of dimension D.
+        - k: The number of nearest neighbors that vote for the predicted labels.
+        - num_loops: Determines which implementation to use to compute distances
+          between training points and testing points.
+
+        Returns:
+        - y: A numpy array of shape (num_test,) containing predicted labels for the
+          test data, where y[i] is the predicted label for the test point X[i].
+        """
+        if num_loops == 0:
+            dis = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dis = self.compute_distances_one_loop(X)
+        elif num_loops == 2:
+            dis = self.compute_distances_two_loops(X)
+        else:
+            raise ValueError('Invalid value %d for num_loops' % num_loops)
+
+        return self.predict_labels(dis)
+
+    def compute_distances_two_loops(self, X):
+        """
+        Compute the distance between each test point in X and each training point
+        in self.X_train using a nested loop over both the training data and the
+        test data.
+
+        Inputs:
+        - X: A numpy array of shape (num_test, D) containing test data.
+
+        Returns:
+        - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
+          is the Euclidean distance between the ith test point and the jth training
+          point.
+        """
+        num_test = X.shape[0]
+        num_train = self.train_X.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i_test in range(num_test):
+            for i_train in range(num_train):
+                dists[i_test, i_train]=np.sum(np.square(X[i_test]-self.train_X[i_train]))
+       
+                # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        return dists
+
+    def compute_distances_one_loop(self, X):
+        """
+        Compute the distance between each test point in X and each training point
+        in self.X_train using a single loop over the test data.
+
+        Input / Output: Same as compute_distances_two_loops
+        """
+        num_test = X.shape[0]
+        num_train = self.train_X.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in range(num_test):
+            dists[i, :] = np.sum(np.square(X[i]-self.train_X), axis=1)
+            # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        return dists
+
+    def compute_distances_no_loops(self, X):
+        """
+        Compute the distance between each test point in X and each training point
+        in self.X_train using no explicit loops.
+
+        Input / Output: Same as compute_distances_two_loops
+        """
+        num_test = X.shape[0]
+        num_train = self.train_X.shape[0]
+        dists = np.zeros((num_test, num_train))
+        #########################################################################
+        # TODO:                                                                 #
+        # Compute the l2 distance between all test points and all training      #
+        # points without using any explicit loops, and store the result in      #
+        # dists.                                                                #
+        #                                                                       #
+        # You should implement this function using only basic array operations; #
+        # in particular you should not use functions from scipy,                #
+        # nor use np.linalg.norm().                                             #
+        #                                                                       #
+        # HINT: Try to formulate the l2 distance using matrix multiplication    #
+        #       and two broadcast sums.                                         #
+        #########################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        a, b = X, self.train_X
+        a = a[:,np.newaxis,:]
+        b = b[np.newaxis,:,:]
+        diff= (a-b)**2
+        dists = np.sqrt(np.sum(diff,axis=2))
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        return dists
+
+    def predict_labels(self, dists):
+        """
+        Given a matrix of distances between test points and training points,
+        predict a label for each test point.
+
+        Inputs:
+        - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
+          gives the distance betwen the ith test point and the jth training point.
+
+        Returns:
+        - y: A numpy array of shape (num_test,) containing predicted labels for the
+          test data, where y[i] is the predicted label for the test point X[i].
+        """
+        num_test = dists.shape[0]
+        y_pred = np.zeros(num_test)
+        for i in range(num_test):
+            # A list of length k storing the labels of the k nearest neighbors to
+            # the ith test point.
+            #########################################################################
+            # TODO:                                                                 #
+            # Use the distance matrix to find the k nearest neighbors of the ith    #
+            # testing point, and use self.y_train to find the labels of these       #
+            # neighbors. Store these labels in closest_y.                           #
+            # Hint: Look up the function numpy.argsort.                             #
+            #########################################################################
+            # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+            closest_y = []
+            ind = np.argsort(dists)[i, ]
+            closest_y = self.train_y[ind][:self.kls]
+            # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+            #########################################################################
+            # TODO:                                                                 #
+            # Now that you have found the labels of the k nearest neighbors, you    #
+            # need to find the most common label in the list closest_y of labels.   #
+            # Store this label in y_pred[i]. Break ties by choosing the smaller     #
+            # label.                                                                #
+            #########################################################################
+            # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+            y_pred[i] = np.bincount(closest_y).argmax()
+            # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        return y_pred
+
+    def predict(self, X, num_loops=0):
+        '''
+        Uses the KNN model to predict classes for the data samples provided
+        
+        Arguments:
+        X, np array (num_samples, num_features) - samples to run
+           through the model
+        num_loops, int - which implementation to use
+
+        Returns:
+        predictions, np array of ints (num_samples) - predicted class
+           for each sample
+        '''
+        if num_loops == 0:
+            dists = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dists = self.compute_distances_one_loop(X)
+        else:
+            dists = self.compute_distances_two_loops(X)
+
+        if self.train_y.dtype == np.bool:
+            return self.predict_labels_binary(dists)
+        else:
+            return self.predict_labels_multiclass(dists)
+
+    def predict_labels_binary(self, dists):
+        '''
+        Returns model predictions for binary classification case
+        
+        Arguments:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+
+        Returns:
+        pred, np array of bool (num_test_samples) - binary predictions 
+           for every test sample
+        '''
+        num_test = dists.shape[0]
+        pred = np.zeros(num_test, np.bool)
+        for i in range(num_test):
+            ix = np.argmin(dists[i,:])
+            pred[i] = self.train_y[ix]
+            
+        return pred
+
+    def predict_labels_multiclass(self, dists):
+        '''
+        Returns model predictions for multi-class classification case
+        
+        Arguments:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+
+        Returns:
+        pred, np array of int (num_test_samples) - predicted class index 
+           for every test sample
+        '''
+        num_test = dists.shape[0]
+        num_test = dists.shape[0]
+        pred = np.zeros(num_test, np.int)
+        for i in range(num_test):
+            # TODO: Implement choosing best class based on k
+            # nearest training samples
+            ix = np.argmin(dists[i,:])
+            pred[i] = self.train_y[ix]
+        return pred
---
+++ metrics.py
@@ -0,0 +1,45 @@
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+
+
+def binary_classification_metrics(prediction, ground_truth):
+    '''
+    Computes metrics for binary classification
+
+    Arguments:
+    prediction, np array of bool (num_samples) - model predictions
+    ground_truth, np array of bool (num_samples) - true labels
+
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    '''
+    # precision = 0
+    # recall = 0
+    # accuracy = 0
+    # f1 = 0
+
+    # TODO: implement metrics!
+    # Some helpful links:
+    # https://en.wikipedia.org/wiki/Precision_and_recall
+    # https://en.wikipedia.org/wiki/F1_score
+    # https://towardsdatascience.com/accuracy-precision-recall-or-f1-331fb37c5cb9
+    precision = precision_score(ground_truth, prediction)
+    recall = recall_score(ground_truth, prediction)
+    f1 = f1_score(ground_truth, prediction)
+    accuracy = accuracy_score(ground_truth, prediction)
+    return precision, recall, f1, accuracy
+
+
+def multiclass_accuracy(prediction, ground_truth):
+    '''
+    Computes metrics for multiclass classification
+
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    '''
+    # TODO: Implement computing accuracy
+    accuracy = accuracy_score(ground_truth, prediction)
+    return accuracy
